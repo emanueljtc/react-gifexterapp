657bf8c64ce95c72c818dbf2122a9e764612ae29
Ing. Emanuel Torres Clemente <emanueljtc@desarrollowebtotal.com>
2020-09-21 23:00:56 -0400

    animated css

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -7,8 +7,14 @@ export const GifGrid = ({ category }) => {
 
   return (
     <>
-      <h3>{category}</h3>
-      {loading && <p>Loading...</p>}
+      <h3 className="animate__animated animate__fadeIn animate__slow">
+        {category}
+      </h3>
+      {loading && (
+        <p className="animate__animated animate__flash animate__slow">
+          Loading...
+        </p>
+      )}
       <div className="card-grid">
         {images.map((img) => (
           <GifGridItem key={img.id} {...img} />

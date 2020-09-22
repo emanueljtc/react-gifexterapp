d82f9e4b98c2fd405291aff08a100f290c3e5564
Ing. Emanuel Torres Clemente <emanueljtc@desarrollowebtotal.com>
2020-09-22 07:56:56 -0400

    docs agregado

---
+++ docs/precache-manifest.ff67a9be7a9cefd025b3355c3b4bfba2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be39a33f917d57772ffbf5749c39e776",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b08d9fc5c13ad4a207db",
+    "url": "/static/css/main.61b12e29.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b08d9fc5c13ad4a207db",
+    "url": "/static/js/main.35b2a546.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);

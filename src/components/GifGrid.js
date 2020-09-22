f7d106339bd0b9f1f500dde65f8d6f3d8a9311a5
Ing. Emanuel Torres Clemente <emanueljtc@desarrollowebtotal.com>
2020-09-21 22:50:25 -0400

    hoosk

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,34 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
-  const [images, setImages] = useState([]);
-  useEffect(() => {
-    getGifs();
-  }, []);
-
-  const getGifs = async () => {
-    const url =
-      'https://api.giphy.com/v1/gifs/search?q=Rick+and+Morty&limit=10&api_key=ziEV2A2BvcXCc1VZ4lqDci1W1Kb5CTY2';
-    const resp = await fetch(url);
-    const { data } = await resp.json();
-    const gifs = data.map((img) => {
-      return {
-        id: img.id,
-        title: img.title,
-        url: img.images?.downsized_medium.url,
-      };
-    });
-    setImages(gifs);
-  };
+  const { data: images, loading } = useFetchGifs(category);
 
   return (
     <>
       <h3>{category}</h3>
-
-      {images.map((image) => (
-        <GifGridItem key={image.id} {...image} />
-      ))}
+      {loading && <p>Loading...</p>}
+      <div className="card-grid">
+        {images.map((img) => (
+          <GifGridItem key={img.id} {...img} />
+        ))}
+      </div>
     </>
   );
 };
